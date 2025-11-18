990c15a2cbca21d91b473d40ed43a7fb7d12d50e
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-11-18 22:36:45 +0545

    fix: remove signal not preferred for concurrency control

--- Backend/Books/signals.py
+++ Backend/Books/signals.py
@@ -1,30 +1,7 @@
-from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in
 from Books.models import Wishlist
 import json
-
-
-@receiver(pre_save, sender='Books.BorrowedBook')    
-def book_borrow_return(sender, instance, **kwargs):
-    borrowed_book = instance
-    bookObj = borrowed_book.book
-    if instance._state.adding:  # new instance being created
-        if not borrowed_book.is_returned and bookObj.is_available:  # on new borrow
-            bookObj.stock -= 1
-            bookObj.save()
-        else: # trying to borrow when no stock available
-            raise ValueError("Book is not available for borrowing.")
-
-    else:   # on update
-        old_instance = sender.objects.get(pk=borrowed_book.pk)
-        if not old_instance.is_returned and borrowed_book.is_returned: # status changed from False to True
-            bookObj.stock += 1      # avoid double incrementing stock if already returned (comparing old and new status)
-            bookObj.save()  # save the updated stock   
-
-    ''' Signal to adjust book stock on borrow/return of books 
-        must be called before saving BorrowedBook instance to have correct stock update
-    '''
 
 
 @receiver(user_logged_in)
@@ -43,3 +20,36 @@ def merge_wishlist_on_login(sender, request, **kwargs):     # no session used in
     wishlist_obj.save()     
 
     print(f"Merged {len(json_wishlist)} wishlist items into user {user.username}'s wishlist on login.")   
+
+
+
+'''
+Signals cannot handle concurrency because:
+    They do not lock database rows
+    They run outside the API control flow
+    They can be triggered unexpectedly (admin panel, shell, tests)
+    They cause inconsistent business logic execution under concurrent requests.
+
+
+    @receiver(pre_save, sender='Books.BorrowedBook')    
+    def book_borrow_return(sender, instance, **kwargs):
+        borrowed_book = instance
+        bookObj = borrowed_book.book
+        if instance._state.adding:  # new instance being created
+            if not borrowed_book.is_returned and bookObj.is_available:  # on new borrow
+                bookObj.stock -= 1
+                bookObj.save()
+            else: # trying to borrow when no stock available
+                raise ValueError("Book is not available for borrowing.")
+
+        else:   # on update
+            old_instance = sender.objects.get(pk=borrowed_book.pk)
+            if not old_instance.is_returned and borrowed_book.is_returned: # status changed from False to True
+                bookObj.stock += 1      # avoid double incrementing stock if already returned (comparing old and new status)
+                bookObj.save()  # save the updated stock   
+
+    Signal to adjust book stock on borrow/return of books 
+    must be called before saving BorrowedBook instance to have correct stock update 
+
+
+'''
